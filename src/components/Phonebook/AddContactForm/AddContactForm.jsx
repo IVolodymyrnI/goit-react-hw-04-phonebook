6c5e877261b572ffc6426ee17ac128d4d9fb33b7
IVolodymyrnI <volodia.iarcguk.forwork@gmail.com>
2023-03-11 12:28:48 +0200

    react-02-phonebook

--- src/components/Phonebook/AddContactForm/AddContactForm.jsx
+++ src/components/Phonebook/AddContactForm/AddContactForm.jsx
@@ -1,14 +1,26 @@
 import { Formik } from 'formik';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
-import { Input, ContactForm, Label, Button } from './AddContactFormStyle';
+import {
+  Input,
+  ContactForm,
+  Label,
+  Button,
+  Error,
+  InputWrapper,
+} from './AddContactFormStyle';
+import { object, string } from 'yup';
 
+const PHONE_NUMBER_PATTERN =
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
 const NAME_PATTERN =
   "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
 const NAME_PATTERN_TITTLE =
   "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan.";
-const PHONENUMBER_PATTERN_TITTLE =
+const PHONE_NUMBER_PATTERN_TITTLE =
   'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +';
+const NAME_PATTERN_TEXT = 'Name is a required field';
+const PHONE_NUMBER_PATTERN_TEXT = 'Phone number is a required field';
 
 export const AddContactForm = ({ onAddContactBtn }) => {
   const phoneNumberId = nanoid();
@@ -20,27 +32,32 @@ export const AddContactForm = ({ onAddContactBtn }) => {
     resetForm();
   };
 
+  const schema = object({
+    name: string()
+      .matches(NAME_PATTERN, NAME_PATTERN_TITTLE)
+      .required(NAME_PATTERN_TEXT),
+    phoneNumber: string()
+      .matches(PHONE_NUMBER_PATTERN, PHONE_NUMBER_PATTERN_TITTLE)
+      .required(PHONE_NUMBER_PATTERN_TEXT),
+  });
+
   return (
-    <Formik initialValues={{ name: '', phoneNumber: '' }} onSubmit={onSubmit}>
+    <Formik
+      initialValues={{ name: '', phoneNumber: '' }}
+      onSubmit={onSubmit}
+      validationSchema={schema}
+    >
       <ContactForm>
         <Label htmlFor={nameId}>Name</Label>
-        <Input
-          type="text"
-          id={nameId}
-          pattern={NAME_PATTERN}
-          title={NAME_PATTERN_TITTLE}
-          name="name"
-          required
-        ></Input>
+        <InputWrapper>
+          <Error name="name" component="p" />
+          <Input type="text" id={nameId} name="name"></Input>
+        </InputWrapper>
         <Label htmlFor={phoneNumberId}>Phone number</Label>
-        <Input
-          type="tel"
-          id={phoneNumberId}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title={PHONENUMBER_PATTERN_TITTLE}
-          name="phoneNumber"
-          required
-        ></Input>
+        <InputWrapper>
+          <Error name="phoneNumber" component="p" />
+          <Input type="tel" id={phoneNumberId} name="phoneNumber"></Input>
+        </InputWrapper>
         <Button type="submit">Add contact</Button>
       </ContactForm>
     </Formik>
--- src/components/Phonebook/AddContactForm/AddContactFormStyle.js
+++ src/components/Phonebook/AddContactForm/AddContactFormStyle.js
@@ -1,4 +1,4 @@
-import { Form, Field } from 'formik';
+import { Form, Field, ErrorMessage } from 'formik';
 import styled from 'styled-components';
 
 export const ContactForm = styled(Form)`
@@ -9,10 +9,25 @@ export const ContactForm = styled(Form)`
   margin-bottom: 30px;
 `;
 
+export const InputWrapper = styled.div`
+  position: relative;
+  margin-bottom: 20px;
+  :last-of-type {
+    margin-bottom: 35px;
+  }
+`;
+
+export const Error = styled(ErrorMessage)`
+  position: absolute;
+  top: 24px;
+  color: red;
+  font-size: 10px;
+`;
+
 export const Label = styled.label`
   display: block;
-  margin-bottom: 12px;
   font-size: 24px;
+  margin-bottom: 8px;
   font-weight: 500;
 `;
 
@@ -20,7 +35,6 @@ export const Input = styled(Field)`
   outline: none;
   border: 1px solid gray;
   display: block;
-  margin-bottom: 16px;
   :focus-within {
     border-color: blue;
     box-shadow: rgba(3, 102, 214, 0.5) 0px 0px 0px 3px;
